139fa561665563ecc4471d193d8e781c8a51ed90
Junior <eecfredes@gmail.com>
2024-07-29 21:18:03 -0300

    :rocket: main file

---
+++ cmd/app/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"templ/cmd/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	router.Get("/foo", handlers.Make(handlers.HandleFoo))
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "listenAddr", listenAddr)
+	http.ListenAndServe(listenAddr, router)
+}
